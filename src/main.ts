eeeb90439b9191fb19700c58b407da7b9bc8c248
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-09 21:37:25 +0100

    Configuring environment variables

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { PORT } = process.env;
+
+  try {
+    await app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
 }
 bootstrap();
