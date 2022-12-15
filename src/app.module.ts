730bfe6a48dd7ec01d593a205ba27f66fb0182c4
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-15 17:00:10 +0100

    Setting up basic auth structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './react-api/api.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ApiModule],
+  imports: [ApiModule, AuthModule, UsersModule],
 })
 export class AppModule {}
