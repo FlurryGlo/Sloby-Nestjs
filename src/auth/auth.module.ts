97476ca831065359382960bd85cedb6f8ba487e6
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-16 17:13:19 +0100

    Refactoring guard & need to fix auth problems

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStratergy } from './utils/local.stratergy';
+import { SessionSerializer } from './utils/SessionSerializer';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStratergy],
+  providers: [AuthService, LocalStratergy, SessionSerializer],
 })
 export class AuthModule {}
--- src/auth/utils/Guards.ts
+++ src/auth/utils/Guards.ts
@@ -4,7 +4,15 @@ import { ExecutionContext } from '@nestjs/common/interfaces';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const result = (await super.canActivate(context)) as boolean;
+    const request = context.switchToHttp().getRequest();
+
+    await super.logIn(request);
+    return result;
+  }
+}
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
