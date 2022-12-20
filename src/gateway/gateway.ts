8cb7d325b2f48db6849f6270604502aa671af4e9
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-20 15:40:49 +0100

    Create websocket server instance

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,8 +1,24 @@
+import { OnModuleInit } from '@nestjs/common';
 import { WebSocketGateway } from '@nestjs/websockets';
-import { MessageBody, SubscribeMessage } from '@nestjs/websockets/decorators';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketServer,
+} from '@nestjs/websockets/decorators';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class SlobyGateway {
+export class SlobyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
   @SubscribeMessage('newShard')
   onNewShard(@MessageBody() body: any) {
     console.log('WebSocketGateway has been initialized');
