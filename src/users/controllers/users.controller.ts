f7932b8de76fbb184e83f16bdf79cd036a0f34a6
gaborhadhazy <gaborhadhazy07@gmail.com>
2022-12-22 13:48:17 +0100

    Getting rid of pipe

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -46,7 +46,6 @@ export class UserController {
   }
 
   @Post('create')
-  @UsePipes(ValidationPipe)
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
   }
